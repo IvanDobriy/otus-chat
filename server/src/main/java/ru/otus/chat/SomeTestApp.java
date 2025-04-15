65edee183c74e54e4f879ffbfe2525d1364faa82
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-04-15 11:38:42 +0300

    prepare getByUserId

--- server/src/main/java/ru/otus/chat/SomeTestApp.java
+++ server/src/main/java/ru/otus/chat/SomeTestApp.java
@@ -1,6 +1,5 @@
 package ru.otus.chat;
 
-import ru.otus.chat.entities.RoleType;
 import ru.otus.chat.jdbc.Model;
 
 import java.io.IOException;
@@ -11,10 +10,13 @@ public class SomeTestApp {
     public static void main(String[] args) throws IOException, SQLException {
         final var model = new Model();
         final var userQuery = model.getUser();
+        final var restrictionQuery = model.getRestriction();
         final var result = userQuery.getUserByLogin("admin");
-        final var roleQuery = model.getRole();
-        roleQuery.create(10L, 1L, RoleType.USER);
-        userQuery.create(10L, "fff", "fff", "fff1");
-        model.save();
+        final var restriction = restrictionQuery.getByUserId(1L);
+        final var a = restriction;
+//        final var roleQuery = model.getRole();
+//        roleQuery.create(10L, 1L, RoleType.USER);
+//        userQuery.create(10L, "fff", "fff", "fff1");
+//        model.save();
     }
 }
